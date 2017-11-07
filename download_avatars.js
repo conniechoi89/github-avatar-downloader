02d81281daa828df2383c5e3e423dabaa8a4f1b6
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-07 04:18:44 +0000

    update

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,5 @@
 const request = require('request');
 const accessToken = require('./secrets');
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
@@ -14,10 +10,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
     headers: {
       'User-Agent': 'request',
       'Authorization': "token " + accessToken.GITHUB_TOKEN
-    }
+    },
+    json: true
   };
-    request(options, function(err, res, body) {
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
+getRepoContributors("jquery", "jquery", function(err, contributors) {
+  console.log("Errors:", err);
+  contributors.forEach(function(contributor) {
+    console.log(contributor.avatar_url);
+  });
+});
