61e46b88c7942751d2a86eb6ec1c513d9d2b2bde
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-07 08:31:20 +0000

    step8

--- download_avatars.js
+++ download_avatars.js
@@ -1,37 +1,48 @@
 const request = require('request');
 const accessToken = require('./secrets');
 const fs = require('fs');
+const repoOwner = process.argv[2];
+const repoName = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
   const options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-
     headers: {
       'User-Agent': 'request',
       'Authorization': "token " + accessToken.GITHUB_TOKEN
     },
     json: true
   };
+
   request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
-// getRepoContributors("jquery", "jquery", function(err, contributors) {
-//   console.log("Errors:", err);
-//   contributors.forEach(function(contributor) {
-//     console.log(contributor.avatar_url);
-//   });
-// });
+function downloadImageByUrl(url, filePath){
+  request.get(url)
+       .on('error', function (err) {
+         throw console.log('rip', err);
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
 
-function downloadImageByURL(url, filePath) {
-  request
-    .get(url)
-    .on('end', () => {console.log("end");})
-    .pipe(fs.createWriteStream(filePath));
-};
+getRepoContributors(repoOwner, repoName, function(err, contributors) {
+  console.log("Errors:", err);
+  contributors.forEach(function(contributor) {
+    var url = contributor.avatar_url;
+    var filePath = 'avatars/' + contributor.login + '.jpg';
+
+    // console.log(contributor.avatar_url);
+    downloadImageByUrl(url, filePath);
+  });
+});
 
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
 
 
